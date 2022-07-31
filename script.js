fb5bb64c44fc0a1a9ef8c3a44a73ae7f72bd0aa4
AKam <andy.kaminski42@gmail.com>
2022-07-31 14:55:05 -0400

    Added checks for adding uppercase, numbers and special characters to the password array

--- script.js
+++ script.js
@@ -13,18 +13,30 @@ function generatePassword() {
   var specialInput = window.confirm("Would you like your password to include special characters?");
   var passwordArray = [];
 
+// Checks if undercase was selected and adds to array if true
   if (undercaseInput) {
     for (i=0; i<letters.length; i++) {
       passwordArray[i] = letters[i].toLowerCase();
     };
   }
-  /*
+// Checks if uppercase was selected and adds to array if true. Also checks to see if array has already been added to yet.
   if (uppercaseInput && passwordArray.length > 0) {
-    passwordArray = passwordArray.join(letters)
+    passwordArray = passwordArray.concat(letters);
   } else if (uppercaseInput) {
     passwordArray = letters;
   }
-*/
+// Checks if numbers were selected and adds to the array if true. Also checks to see if array has already been added to yet.
+  if (numbersInput && passwordArray.length > 0) {
+    passwordArray = passwordArray.concat(nums);
+  } else if (numbersInput) {
+    passwordArray = nums;
+  }
+// Checks if special characters were selected and adds to the array if true. Also checks to see if array has already been added to yet.
+if (specialInput && passwordArray.length > 0) {
+  passwordArray = passwordArray.concat(specialChars);
+} else if (specialInput) {
+  passwordArray = specialChars;
+}
   return passwordArray;
 }
 
