53413a85ff368728ff94d5e5e52995883b343f18
AKam <andy.kaminski42@gmail.com>
2022-07-31 11:50:26 -0400

    started the generate password function/ defined variables for user inputs

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 var letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","R","S","T","U","V","W","X","Y","Z"];
 var nums = [0,1,2,3,4,5,6,7,8,9];
 var specialChars = ["!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","\"","^","_","`","{","|","}","~"];
 
+function generatePassword() {
+  var passLengthInput = window.prompt("How many characters do you want your password? (Must be between 8-128): ");
+  var undercaseInput = window.confirm("Would you like your password to include undercase letters?");
+  var uppercaseInput = window.confirm("Would you like your password to include uppercase letters?");
+  var numbersInput = window.confirm("Would you like your password to include numbers?");
+  var specialInput = window.confirm("Would you like your password to include special characters?");
+  var passwordArray = [];
 
 // Write password to the #password input
 function writePassword() {
